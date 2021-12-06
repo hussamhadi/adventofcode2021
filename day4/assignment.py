9baa95c7da9f6ca5668b34e2b8f19ad855da1372
Hussam Hadi <hussam.hadi@teamrockstars.nl>
2021-12-06 01:47:59 +0100

    day 4

--- day4/assignment.py
+++ day4/assignment.py
@@ -21,13 +21,32 @@ def day4_1():
                     return sum(set(board) - drawn_set) * drawn_number
 
 
+def day4_2():
+    board_length = len(boards[0])
+    drawn_set = set(numbers[:4])
+    for drawn_number in numbers[4:]:
+        drawn_set.add(drawn_number)
+        for x, board in enumerate(boards):
+            for i in range(0, board_length, 5):
+                current_row_set = set(board[i:i+5])
+                current_column_set = set(board[int(i/5)::5])
+                if len(boards) > 1:
+                    if current_row_set.issubset(drawn_set) or current_column_set.issubset(drawn_set):
+                        boards.pop(x)
+                else:
+                    if current_row_set.issubset(drawn_set):
+                        return sum(set(board) - drawn_set) * drawn_number
+                    if current_column_set.issubset(drawn_set):
+                        return sum(set(board) - drawn_set) * drawn_number
+
+
 if __name__ == '__main__':
     durations = []
-    no_of_runs = 1
-    print_result = True
+    no_of_runs = 1000
+    print_result = False
     for i in range(no_of_runs):
         start = datetime.now()
-        result = day4_1()
+        result = day4_2()
         if print_result:
             print("answer is {}".format(result))
         end = datetime.now()
